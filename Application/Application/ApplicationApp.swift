c2ff5bccd1fdebcc3e5e9fca414de00173f9666a
Marek Loose <loose.marek@gmail.com>
2022-11-11 18:18:59 +0100

    Added Application (main) project to workspace

---
+++ Application/Application/ApplicationApp.swift
@@ -0,0 +1,17 @@
+//
+//  ApplicationApp.swift
+//  Application
+//
+//  Created by Marek Loose on 11/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct ApplicationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
