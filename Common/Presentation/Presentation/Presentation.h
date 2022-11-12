0a17dc40c2bbecf850ffc10133686937c1f97b34
Marek Loose <loose.marek@gmail.com>
2022-11-12 13:19:39 +0100

    Added Presentation framework

---
+++ Common/Presentation/Presentation/Presentation.h
@@ -0,0 +1,18 @@
+//
+//  Presentation.h
+//  Presentation
+//
+//  Created by Marek Loose on 12/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Presentation.
+FOUNDATION_EXPORT double PresentationVersionNumber;
+
+//! Project version string for Presentation.
+FOUNDATION_EXPORT const unsigned char PresentationVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Presentation/PublicHeader.h>
+
+
