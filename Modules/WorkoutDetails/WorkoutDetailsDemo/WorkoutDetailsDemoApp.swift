bc2fb5a9757ec50cf3499b9705c6085b53366084
Marek Loose <loose.marek@gmail.com>
2022-11-20 13:32:10 +0100

    Added WorkoutDetailsDemo App to WorkoutDetails module

---
+++ Modules/WorkoutDetails/WorkoutDetailsDemo/WorkoutDetailsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutDetailsDemoApp.swift
+//  WorkoutDetailsDemo
+//
+//  Created by Marek Loose on 20/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutDetailsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
