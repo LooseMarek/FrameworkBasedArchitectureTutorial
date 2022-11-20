dbd0a6814c8bc7db2b98fece32401eb357fd99ce
Marek Loose <loose.marek@gmail.com>
2022-11-20 13:23:22 +0100

    Added WorkoutLogs module

---
+++ Modules/WorkoutLogs/WorkoutLogs/WorkoutLogs.h
@@ -0,0 +1,18 @@
+//
+//  WorkoutLogs.h
+//  WorkoutLogs
+//
+//  Created by Marek Loose on 20/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkoutLogs.
+FOUNDATION_EXPORT double WorkoutLogsVersionNumber;
+
+//! Project version string for WorkoutLogs.
+FOUNDATION_EXPORT const unsigned char WorkoutLogsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkoutLogs/PublicHeader.h>
+
+
