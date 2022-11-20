30bcf402388bdd09eba2da381dcc50b0a8ecd874
Marek Loose <loose.marek@gmail.com>
2022-11-20 13:29:22 +0100

    Added WorkoutLogsDemo App to WorkoutLogs module

---
+++ Modules/WorkoutLogs/WorkoutLogsDemo/WorkoutLogsDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkoutLogsDemoApp.swift
+//  WorkoutLogsDemo
+//
+//  Created by Marek Loose on 20/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct WorkoutLogsDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
