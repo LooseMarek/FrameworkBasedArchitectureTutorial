9375f1a60df7a26d71d7b75a6ed9bd5bd0d3da7e
Marek Loose <loose.marek@gmail.com>
2022-11-20 13:30:31 +0100

    Added WorkoutDetails module

---
+++ Modules/WorkoutDetails/WorkoutDetails/WorkoutDetails.h
@@ -0,0 +1,18 @@
+//
+//  WorkoutDetails.h
+//  WorkoutDetails
+//
+//  Created by Marek Loose on 20/11/2022.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for WorkoutDetails.
+FOUNDATION_EXPORT double WorkoutDetailsVersionNumber;
+
+//! Project version string for WorkoutDetails.
+FOUNDATION_EXPORT const unsigned char WorkoutDetailsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WorkoutDetails/PublicHeader.h>
+
+
